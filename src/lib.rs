4c8d766b8a8859609e048b3e3dea40455809ae94
Lenard0 <lachie.hislop@gmail.com>
2024-08-12 19:41:17 +0930

    fixed minor bug

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ pub fn turn_type_to_function_call(input: TokenStream) -> TokenStream {
                 impl open_ai_rust::logoi::input::tool::raw_macro::FunctionCallable for #name {
                     fn to_fn_call(&self) -> FunctionCall {
                         FunctionCall {
-                            name: stringify!(#name),
+                            name: String::from(stringify!(#name)),
                             description: None,
                             parameters: vec![
                                 #(
