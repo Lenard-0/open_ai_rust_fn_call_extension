b67ffb7eabc6cc89046bd26d5a4b7541391f8faf
Lenard0 <lachie.hislop@gmail.com>
2024-08-12 18:55:23 +0930

    more bug fixes

--- src/lib.rs
+++ src/lib.rs
@@ -13,11 +13,11 @@ pub fn turn_type_to_function_call(input: TokenStream) -> TokenStream {
         Data::Struct(s) => {
             let fields = s.fields.into_iter().map(|field| field.ident.unwrap());
             quote! {
-                impl json_trait::Json for #name {
+                impl open_ai_rust::FunctionCallable for #name {
                     fn to_json(&self) -> String {
                         let mut json = "{ ".to_string();
                         #(
-                            json.push_str(&format!("\"{}\": {}, ", stringify!(#fields), json_trait::Json::to_json(&self.#fields)));
+                            json.push_str(&format!("\"{}\": {}, ", stringify!(#fields), open_ai_rust::FunctionCallable::to_fn_call(&self.#fields)));
                         )*
                         json.remove(json.len() - 2); // remove trailling comma
                         json.push('}');
